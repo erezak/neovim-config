070fc683110978f4381734b88769bdc33a7baa67
Erez A. Korn <ek844y@intl.att.com>
2024-04-06 00:12:24 +0300

    Initial config

---
+++ lua/plugins/alpha.lua
@@ -0,0 +1,15 @@
+return {
+	"goolord/alpha-nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		local alpha = require("alpha")
+		local dashboard = require("alpha.themes.startify")
+		dashboard.section.header.val = {
+			[[  ____|   ]], 
+			[[  __|    __| _ \_  /  ]], 
+			[[  |     |    __/  /   ]],
+			[[ _____|_|  \___|___| ]],
+		}
+    alpha.setup(dashboard.opts)
+	end,
+}
---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,79 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require('mason-lspconfig').setup({
+        ensure_installed = {
+          "lua_ls",
+          "pyright",
+          "tsserver",
+          "clangd",
+          "gopls",
+        }
+      })
+    end
+  },
+  {
+    "neovim/nvim-lspconfig",
+    lazy = false,
+    config = function()
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+      local lspconfig = require('lspconfig')
+
+      lspconfig.tsserver.setup({
+        capabilities = capabilities
+      })
+      lspconfig.pyright.setup({
+        capabilies = capabilities
+      })
+
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities,
+        on_init = function(client)
+          local path = client.workspace_folders[1].name
+          if vim.loop.fs_stat(path..'/.luarc.json') or vim.loop.fs_stat(path..'/.luarc.jsonc') then
+            return
+          end
+
+          client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+            runtime = {
+              -- Tell the language server which version of Lua you're using
+              -- (most likely LuaJIT in the case of Neovim)
+              version = 'LuaJIT'
+            },
+            -- Make the server aware of Neovim runtime files
+            workspace = {
+              checkThirdParty = false,
+              library = {
+                vim.env.VIMRUNTIME
+              }
+            }
+          })
+        end,
+        settings = {
+          Lua = {}
+        }
+      })
+
+      vim.api.nvim_create_autocmd('LspAttach', {
+        group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+        callback = function(ev)
+          -- Enable completion triggered by <c-x><c-o>
+          vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+          local opts = { buffer = ev.buf }
+          vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, {})
+          vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+          vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+          vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
+        end,
+      })
+    end,
+  }
+}
