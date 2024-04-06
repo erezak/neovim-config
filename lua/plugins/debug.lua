964614d6fe091f0edcd5a8590c1c2b370da57f3d
Erez A. Korn <ek844y@intl.att.com>
2024-04-06 07:37:56 +0300

    Added python debugger

--- lua/plugins/debug.lua
+++ lua/plugins/debug.lua
@@ -1,34 +1,46 @@
 return {
-  "mfussenegger/nvim-dap",
-  dependencies = {
-    "rcarriga/nvim-dap-ui",
-    "nvim-neotest/nvim-nio",
-    "leoluz/nvim-dap-go",
+  {
+    "mfussenegger/nvim-dap",
+    dependencies = {
+      "rcarriga/nvim-dap-ui",
+      "nvim-neotest/nvim-nio",
+      "leoluz/nvim-dap-go",
+    },
+    config = function()
+      local dap = require("dap")
+      local dapui = require("dapui")
+
+      dapui.setup()
+      require("dap-go").setup()
+
+      dap.listeners.before.attach.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.launch.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated.dapui_config = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited.dapui_config = function()
+        dapui.close()
+      end
+      vim.keymap.set("n", "<Leader>dt", function()
+        dap.toggle_breakpoint()
+      end)
+      vim.keymap.set("n", "<Leader>dc", function()
+        dap.continue()
+      end)
+    end,
   },
-  config = function()
-    local dap = require("dap")
-    local dapui = require("dapui")
-
-    dapui.setup()
-    require("dap-go").setup()
-
-    dap.listeners.before.attach.dapui_config = function()
-      dapui.open()
-    end
-    dap.listeners.before.launch.dapui_config = function()
-      dapui.open()
-    end
-    dap.listeners.before.event_terminated.dapui_config = function()
-      dapui.close()
-    end
-    dap.listeners.before.event_exited.dapui_config = function()
-      dapui.close()
-    end
-    vim.keymap.set("n", "<Leader>dt", function()
-      dap.toggle_breakpoint()
-    end)
-    vim.keymap.set("n", "<Leader>dc", function()
-      dap.continue()
-    end)
-  end,
+  {
+    "mfussenegger/nvim-dap-python",
+    dependencies = {
+      "mfussenegger/nvim-dap-python",
+    },
+    config = function()
+      local python_dap = require("dap-python")
+      python_dap.setup("~/.virtualenvs/debugpy/bin/python")
+    end,
+  },
 }
