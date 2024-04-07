64e4b06b627ba5c6793422ef836982e47f9b9fcc
Erez A. Korn <ek844y@intl.att.com>
2024-04-07 14:50:11 +0300

    Disabled arrow keys in normal mode

--- lua/settings.lua
+++ lua/settings.lua
@@ -1,6 +1,15 @@
+vim.g.loaded_netrwPlugin = 1
+vim.g.loaded_netrw = 1
+
 vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set tabstop=2")
 vim.cmd("set shiftwidth=2")
 
 vim.g.mapleader = ' '
+
+-- Disable arrow keys in normal mode
+vim.keymap.set('n', '<Up>', '<Nop>', {noremap = true, silent = true})
+vim.keymap.set('n', '<Down>', '<Nop>', {noremap = true, silent = true})
+vim.keymap.set('n', '<Left>', '<Nop>', {noremap = true, silent = true})
+vim.keymap.set('n', '<Right>', '<Nop>', {noremap = true, silent = true})
