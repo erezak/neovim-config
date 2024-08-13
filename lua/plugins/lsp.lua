cbded4ccb0090e623a3ac2caefd0ba1a982a68f7
Erez A. Korn <ek844y@intl.att.com>
2024-08-13 11:34:35 +0300

    Added trouble diagnostics and zls local build variable

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -61,6 +61,11 @@ return {
         }
       })
 
+      lspconfig.zls.setup({
+        capabilities = capabilities,
+        cmd = { os.getenv('ZLS_BUILD_PATH') or 'zls' }
+      })
+
       vim.api.nvim_create_autocmd('LspAttach', {
         group = vim.api.nvim_create_augroup('UserLspConfig', {}),
         callback = function(ev)
