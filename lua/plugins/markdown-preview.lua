bc8dd5dbe24bfd7f54098b299d3dac2719b53a2a
Erez A. Korn <ek844y@intl.att.com>
2024-10-31 14:38:00 +0200

    Fixed minor warning

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -1,9 +1,9 @@
-function dump(o)
+function Dump(o)
   if type(o) == 'table' then
     local s = '{ '
     for k, v in pairs(o) do
       if type(k) ~= 'number' then k = '"' .. k .. '"' end
-      s = s .. '[' .. k .. '] = ' .. dump(v) .. ','
+      s = s .. '[' .. k .. '] = ' .. Dump(v) .. ','
     end
     return s .. '} '
   else
