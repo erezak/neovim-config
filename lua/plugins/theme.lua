e725e857f5d05b2fff1b3f68d7af5e291fe87670
Erez A. Korn <ek844y@intl.att.com>
2024-11-05 17:11:39 +0200

    Semantic rename of plugins

---
+++ lua/plugins/theme.lua
@@ -0,0 +1,24 @@
+return {
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		lazy = false,
+		config = function()
+			-- vim.cmd.colorscheme("catppuccin")
+		end,
+	},
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {},
+		config = function()
+			-- vim.cmd.colorscheme("tokyonight")
+			vim.cmd.colorscheme("tokyonight-night")
+			-- vim.cmd.colorscheme("tokyonight-storm")
+			-- vim.cmd.colorscheme("tokyonight-day")
+			-- vim.cmd.colorscheme("tokyonight-moon")
+		end,
+	},
+}
