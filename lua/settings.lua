5ce96710f7ccb26648feb1041013ee6a1676ef5b
Erez A. Korn <ek844y@intl.att.com>
2024-11-05 17:35:30 +0200

    Disable mouse

--- lua/settings.lua
+++ lua/settings.lua
@@ -14,6 +14,8 @@ vim.keymap.set('n', '<Up>', '<Nop>', {noremap = true, silent = true})
 vim.keymap.set('n', '<Down>', '<Nop>', {noremap = true, silent = true})
 vim.keymap.set('n', '<Left>', '<Nop>', {noremap = true, silent = true})
 vim.keymap.set('n', '<Right>', '<Nop>', {noremap = true, silent = true})
+-- and mouse
+vim.opt.mouse = ""
 
 -- set line numbers
 vim.opt.number = true
