e5afcad6626cf7e79accdbe2aa2f39198022b91c
Erez A. Korn <ek844y@intl.att.com>
2025-06-10 09:38:10 +0300

    Moved over to neogit and gitsigns

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,20 +1,36 @@
 return {
-	"kdheepak/lazygit.nvim",
-	lazy = true,
-	cmd = {
-		"LazyGit",
-		"LazyGitConfig",
-		"LazyGitCurrentFile",
-		"LazyGitFilter",
-		"LazyGitFilterCurrentFile",
-	},
-	-- optional for floating window border decoration
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-	},
-	-- setting the keybinding for LazyGit with 'keys' is recommended in
-	-- order to load the plugin when the command is run for the first time
-	keys = {
-		{ "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
-	},
-} 
+  {
+    "lewis6991/gitsigns.nvim",
+    config = true,
+    lazy = false,
+    keys = {
+      { "]c", "<cmd>Gitsigns next_hunk<CR>", desc = "Next Hunk" },
+      { "[c", "<cmd>Gitsigns prev_hunk<CR>", desc = "Prev Hunk" },
+      { "<leader>gh", "<cmd>Gitsigns preview_hunk<CR>", desc = "Preview Hunk" },
+      { "<leader>gb", "<cmd>Gitsigns blame_line<CR>", desc = "Blame Line" },
+      { "<leader>gd", "<cmd>Gitsigns diffthis<CR>", desc = "Diff This" },
+      { "<leader>gD", "<cmd>Gitsigns diffthis HEAD<CR>", desc = "Diff This HEAD" },
+      { "<leader>gr", "<cmd>Gitsigns reset_hunk<CR>", desc = "Reset Hunk" },
+      { "<leader>gR", "<cmd>Gitsigns reset_buffer<CR>", desc = "Reset Buffer" },
+    },
+  },
+  {
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "sindrets/diffview.nvim",
+
+      -- picker
+      "nvim-telescope/telescope.nvim",
+    },
+    keys = {
+      { "<leader>gs", "<cmd>Neogit<CR>", desc = "Neogit" },
+      { "<leader>gS", "<cmd>Neogit kind=split<CR>", desc = "Neogit Split" },
+      { "<leader>gV", "<cmd>Neogit kind=vsplit<CR>", desc = "Neogit VSplit" },
+      { "<leader>gc", "<cmd>Neogit commit<CR>", desc = "Neogit Commit" },
+      { "<leader>gp", "<cmd>Neogit pull<CR>", desc = "Neogit Pull" },
+      { "<leader>gP", "<cmd>Neogit push<CR>", desc = "Neogit Push" },
+      { "<leader>gB", "<cmd>:Telescope git_branches<CR>", desc="Git Branches" },
+    },
+  },
+}
